b7db4f5638121170d57ad45c507308914b267440
Steve Sloka <slokas@vmware.com>
2019-06-18 21:13:18 -0400

    Update to pull from API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,58 @@ import React from 'react';
 import logo from './banana.jpg';
 import './App.css';
 
+class FetchTeam extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://api.pixelcorp.dev/team")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result,
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                {item.firstName} {item.lastName}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+}
+
 function App() {
   return (
     <div className="App">
@@ -12,14 +64,8 @@ function App() {
         <p>
           Welcome to <code>PixelCorp</code>
         </p>
-        <a
-          className="App-link"
-          href="https://stevesloka.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Team:</h3>
+        <FetchTeam />
       </header>
     </div>
   );
